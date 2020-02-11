707733564c16fc4634b1e4c5cb6c2ed0febd7b73
EenRareMan <57411178+EenRareMan@users.noreply.github.com>
2020-02-11 18:43:42 +0100

    Create ticketClose.js

---
+++ commands/ticketClose.js
@@ -0,0 +1,31 @@
+const discord = require("discord.js");
+ 
+module.exports.run = async (bot, message, args) => {
+ 
+    // Id van category van tickets.
+    const categoryId = "663027891256426506";
+ 
+    // Als bericht in ticket kanaal is dan verwijder kanaal ander zend bericht
+    if (message.channel.parentID == categoryId) {
+ 
+        message.channel.delete();
+ 
+    } else {
+ 
+        message.channel.send("Gelieve dit commando in een ticket kanaal te doen.");
+ 
+    }
+ 
+ 
+    // Vind kanaal voor de logs.
+    //var logChannel = message.guild.channels.find("name", "log");
+    //if (!logChannel) return message.channel.send("Kanaal bestaat niet");
+ 
+    //logChannel.send("");
+ 
+}
+ 
+module.exports.help = {
+    name: "close",
+    description: "Sluit een ticket af"
+}
