afc6c3ccc333b798706e7fd654fbc7ade9bc8a35
EenRareMan <57411178+EenRareMan@users.noreply.github.com>
2020-02-11 19:02:44 +0100

    dd
    sdsd

--- commands/ticket.js
+++ commands/ticket.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     // ID van de categorie van de tickets.
-    const categoryId = "663027891256426506";
+    const categoryId = "676850444257198110";
 
     // ID van de roles
     // var roleID = message.guild.roleId;
